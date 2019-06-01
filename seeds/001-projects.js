0bda1cdc7fd6407a9128acc7b02d00c485ab3385
Kristea Gamel <krgamel@gmail.com>
2019-06-01 12:40:42 -0500

    seeds completed

--- seeds/001-projects.js
+++ seeds/001-projects.js
@@ -1,13 +1,14 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('Projects').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('Projects').insert([
+        {id: 1, name: 'Take Care of Plants', description: 'Water, Prune, Rotate all Plants', completed: 'false'},
+        {id: 2, name: 'Clean the House', description: 'Get the house in check', completed: 'false'},
+        {id: 3, name: 'Go to Lowes', description: 'Buy more plants', completed: 'false'},
+    
       ]);
     });
 };
--- seeds/002-actions.js
+++ seeds/002-actions.js
@@ -1,13 +1,19 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('Actions').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('Actions').insert([
+        {id: 1, description: 'prune dead leaves', notes: 'fiddleleaf fig had a few dead leaves', completed: 'true', project_id: '1'},
+        {id: 2, description: 'water plants with fertilizer', notes: 'use miracle grow', completed: 'false', project_id: '1'},
+
+        {id: 3, description: 'dust all the surfaces', notes: 'living room, bedrooms, office, laundry room, bonus room', completed: 'false', project_id: '2'},
+        {id: 4, description: 'sweep', notes: '', completed: 'false', project_id: '2'},
+
+        {id: 5, description: 'buy pink pampas grass', notes: 'pink or purple', completed: 'false', project_id: '3'},
+        {id: 6, description: 'buy ant killer', notes: '', completed: 'false', project_id: '3'},
+       
       ]);
     });
 };
