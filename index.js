e6f73be33431b9adc5ae41eff52706a8e4e15e6c
Kristea Gamel <krgamel@gmail.com>
2019-06-01 12:13:29 -0500

    post an get enpointsdone

--- index.js
+++ index.js
@@ -16,13 +16,62 @@ server.get('/', (req, res) => {
     res.status(200).json({ hello: 'World!' });
   });
 
-//POST endpoint for Projects
+//POST endpoint for projects
+server.post('/api/projects', (req, res) => {
+    const project = req.body;
+    if(!project.name || !project.description) {
+        res.status(409).json({err:'must add required project fields'})
+    }
+    else{
+    db('Projects').insert(project)
+    .then(projectData => {
+        res.status(201).json(projectData);
+    })
+    .catch(err => {
+        res.status(500).json({err:'unable to add data'})
+    })};
+});
 
 //POST endpoint for actions
+server.post('/api/actions', (req, res) => {
+    const action = req.body;
+    db('Actions').insert(action)
+        .then(actionData => {
+        res.status(201).json(actionData);
+    })
+    .catch(err => {
+        res.status(500).json({err:'cannot add action'})
+    });
+});
 
 //GET endpoint for Projects
-
+server.get('/api/projects', (req, res) => {
+    db('Projects')
+    .then(projectId => {res.json(projectId)})
+    .catch(err => {res.status(500).json({message: 'there was an error'})})
+})
 // Get for project by id
+server.get('/api/projects/:id', (req, res) => {
+    const {id} = req.params;
+    db('Projects').where('id', id)
+    .then(projectId => {
+        const project_obj = projectId
+        console.log('Project Obj One', project_obj)
+        db('Actions')
+    .then(actions => {
+            console.log('Type Id', typeof id)
+            console.log('Type action Id', typeof actions[0].id)
+            const project_actions = actions.filter(action => action.project_id === Number(id))
+            console.log('Actions', actions)
+            console.log('Project Actions', project_actions)
+            project_obj[0]['actions'] = project_actions
+            console.log('Project Obj Two', project_obj)
+            res.json(project_obj)
+        })
+    .catch(err => {res.status(500).json({err: 'action could not be retrieved'})})
+    })
+    .catch(err => {res.status(500).json({message: 'project could not be retrieved'})})
+})
 
 server.listen(PORT, () =>{
     console.log(`\nAPI running on port ${PORT}\n`)
--- knexfile.js
+++ knexfile.js
@@ -5,17 +5,17 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/dev.sqlite3'
     }
   },
 
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations'
+      directory: './migrations'
     },
 
     seeds: {
-        directory: './data/seeds'
+        directory: './seeds'
     },
   };
 
