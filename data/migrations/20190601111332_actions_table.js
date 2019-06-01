bb7977d920ead15050ffa7e954e9577f22a45377
Kristea Gamel <krgamel@gmail.com>
2019-06-01 11:37:24 -0500

    actions and project table migration

---
+++ data/migrations/20190601111332_actions_table.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('Actions', table => {
+        table.increments();
+        table.string('description',200).notNullable();
+        table.string('notes',200);
+        table.boolean('completed');
+        
+        table.integer('project_id').unsigned().notNullable();
+        table.foreign('project_id').references('id').inTable('Projects');
+})
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('Actions');
+};
